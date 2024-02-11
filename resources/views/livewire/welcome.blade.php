37e2e680efe601c73e0918d87703b25b1ea8c8a0
Natanael A. de Morais <natanaelallves@gmail.com>
2024-02-11 14:10:33 -0300

    installing tailwindcss

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,3 +1,3 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+<div class="text-red-300">
+    Care about people's approval and you will be their prisoner.
 </div>
